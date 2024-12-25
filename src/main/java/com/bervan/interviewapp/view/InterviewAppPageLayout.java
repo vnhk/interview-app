fe510553e454c4fbd0eaa3fcea4336a23261720b
berluk12@gmail.com <berluk12@gmail.com>
2024-12-25 15:08:15 +0100

    Navigation menu addButtonIfVisible

--- src/main/java/com/bervan/interviewapp/view/InterviewAppPageLayout.java
+++ src/main/java/com/bervan/interviewapp/view/InterviewAppPageLayout.java
@@ -7,13 +7,13 @@ public final class InterviewAppPageLayout extends MenuNavigationComponent {
     public InterviewAppPageLayout(String currentRouteName) {
         super(currentRouteName);
 
-        addButton(menuButtonsRow, AbstractInterviewHomeView.ROUTE_NAME, "Home");
-        addButton(menuButtonsRow, AbstractStartInterviewView.ROUTE_NAME, "Interview!");
-        addButton(menuButtonsRow, AbstractInterviewQuestionsView.ROUTE_NAME, "Interview Questions");
-        addButton(menuButtonsRow, AbstractCodingTaskView.ROUTE_NAME, "Coding Tasks");
-        addButton(menuButtonsRow, AbstractInterviewPlanView.ROUTE_NAME, "Interview Plan");
-        addButton(menuButtonsRow, AbstractQuestionConfigView.ROUTE_NAME, "Question Config");
-        addButton(menuButtonsRow, AbstractImportExportView.ROUTE_NAME, "Import/Export");
+        addButtonIfVisible(menuButtonsRow, AbstractInterviewHomeView.ROUTE_NAME, "Home");
+        addButtonIfVisible(menuButtonsRow, AbstractStartInterviewView.ROUTE_NAME, "Interview!");
+        addButtonIfVisible(menuButtonsRow, AbstractInterviewQuestionsView.ROUTE_NAME, "Interview Questions");
+        addButtonIfVisible(menuButtonsRow, AbstractCodingTaskView.ROUTE_NAME, "Coding Tasks");
+        addButtonIfVisible(menuButtonsRow, AbstractInterviewPlanView.ROUTE_NAME, "Interview Plan");
+        addButtonIfVisible(menuButtonsRow, AbstractQuestionConfigView.ROUTE_NAME, "Question Config");
+        addButtonIfVisible(menuButtonsRow, AbstractImportExportView.ROUTE_NAME, "Import/Export");
 
         add(menuButtonsRow);
     }
