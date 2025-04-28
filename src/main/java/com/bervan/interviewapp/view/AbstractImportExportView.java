e2b6a6143b8efbed5799b3000618fead264d5ff0
berluk12@gmail.com <berluk12@gmail.com>
2025-04-28 21:55:17 +0200

    Common AbstractFilters view, import export data fix, out of memory fix

--- src/main/java/com/bervan/interviewapp/view/AbstractImportExportView.java
+++ src/main/java/com/bervan/interviewapp/view/AbstractImportExportView.java
@@ -1,26 +1,20 @@
 package com.bervan.interviewapp.view;
 
-import com.bervan.common.AbstractDataIEView;
-import com.bervan.common.EmptyLayout;
+import com.bervan.common.AbstractPageView;
 import com.bervan.common.model.PersistableTableData;
 import com.bervan.common.service.BaseService;
 import com.bervan.core.model.BervanLogger;
-import com.bervan.interviewapp.interviewquestions.InterviewQuestionService;
-import com.bervan.interviewapp.interviewquestions.Question;
-import com.bervan.interviewapp.codingtask.CodingTask;
-import com.bervan.interviewapp.questionconfig.QuestionConfig;
 
 import java.util.List;
 import java.util.UUID;
 
 
-public abstract class AbstractImportExportView extends AbstractDataIEView<UUID> {
+public abstract class AbstractImportExportView extends AbstractPageView {
     public static final String ROUTE_NAME = "interview-app/import-export-data";
 
     public AbstractImportExportView(List<BaseService<UUID, ? extends PersistableTableData<?>>> dataServices, BervanLogger logger) {
-        super(dataServices, new EmptyLayout(), logger,
-                List.of(Question.class,
-                        QuestionConfig.class,
-                        CodingTask.class));
+        super();
+
+        showErrorNotification("Not implemented!");
     }
 }
