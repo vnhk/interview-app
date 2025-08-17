17b9371fd4f7f62eda7b6d6f66d3ca37b5a13ff3
berluk12@gmail.com <berluk12@gmail.com>
2025-08-17 14:35:07 +0200

    New Navigation menu icons

--- src/main/java/com/bervan/interviewapp/view/InterviewAppPageLayout.java
+++ src/main/java/com/bervan/interviewapp/view/InterviewAppPageLayout.java
@@ -7,15 +7,13 @@ public final class InterviewAppPageLayout extends MenuNavigationComponent {
 
     public InterviewAppPageLayout(String currentRouteName) {
         super(currentRouteName);
-
         addButtonIfVisible(menuButtonsRow, AbstractInterviewHomeView.ROUTE_NAME, "Home", VaadinIcon.HOME.create());
-        addButtonIfVisible(menuButtonsRow, AbstractStartInterviewView.ROUTE_NAME, "Interview!", VaadinIcon.HOME.create());
-        addButtonIfVisible(menuButtonsRow, AbstractInterviewQuestionsView.ROUTE_NAME, "Interview Questions", VaadinIcon.HOME.create());
-        addButtonIfVisible(menuButtonsRow, AbstractCodingTaskView.ROUTE_NAME, "Coding Tasks", VaadinIcon.HOME.create());
-        addButtonIfVisible(menuButtonsRow, AbstractInterviewPlanView.ROUTE_NAME, "Interview Plan", VaadinIcon.HOME.create());
-        addButtonIfVisible(menuButtonsRow, AbstractQuestionConfigView.ROUTE_NAME, "Question Config", VaadinIcon.HOME.create());
-        addButtonIfVisible(menuButtonsRow, AbstractImportExportView.ROUTE_NAME, "Import/Export", VaadinIcon.HOME.create());
-
+        addButtonIfVisible(menuButtonsRow, AbstractStartInterviewView.ROUTE_NAME, "Interview!", VaadinIcon.PLAY.create());
+        addButtonIfVisible(menuButtonsRow, AbstractInterviewQuestionsView.ROUTE_NAME, "Interview Questions", VaadinIcon.QUESTION.create());
+        addButtonIfVisible(menuButtonsRow, AbstractCodingTaskView.ROUTE_NAME, "Coding Tasks", VaadinIcon.CODE.create());
+        addButtonIfVisible(menuButtonsRow, AbstractInterviewPlanView.ROUTE_NAME, "Interview Plan", VaadinIcon.CALENDAR.create());
+        addButtonIfVisible(menuButtonsRow, AbstractQuestionConfigView.ROUTE_NAME, "Question Config", VaadinIcon.COG.create());
+        addButtonIfVisible(menuButtonsRow, AbstractImportExportView.ROUTE_NAME, "Import/Export", VaadinIcon.UPLOAD.create());
         add(menuButtonsRow);
     }
 }
