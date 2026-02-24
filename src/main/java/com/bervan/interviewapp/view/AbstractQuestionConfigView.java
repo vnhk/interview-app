341c5a5a15e84c401217119bce395d026d14df8c
berluk12@gmail.com <berluk12@gmail.com>
2026-02-24 17:53:44 +0100

    Refactor `AbstractQuestionConfigView` to simplify logic and enhance error handling.
    - Removed column-specific configurations and redundant methods.
    - Integrated `JsonLogger` for enhanced logging and error tracking.
    - Improved error handling with notifications

--- src/main/java/com/bervan/interviewapp/view/AbstractQuestionConfigView.java
+++ src/main/java/com/bervan/interviewapp/view/AbstractQuestionConfigView.java
@@ -4,121 +4,25 @@ import com.bervan.common.config.BervanViewConfig;
 import com.bervan.common.view.AbstractBervanTableView;
 import com.bervan.interviewapp.questionconfig.QuestionConfig;
 import com.bervan.interviewapp.questionconfig.QuestionConfigService;
+import com.bervan.logging.JsonLogger;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dialog.Dialog;
-import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.IntegerField;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.component.textfield.TextFieldBase;
-import com.vaadin.flow.data.renderer.ComponentRenderer;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.UUID;
 
 public abstract class AbstractQuestionConfigView extends AbstractBervanTableView<UUID, QuestionConfig> {
     public static final String ROUTE_NAME = "interview-app/question-config";
-
+    private static final JsonLogger log = JsonLogger.getLogger(AbstractQuestionConfigView.class, "interview-app");
 
     public AbstractQuestionConfigView(@Autowired QuestionConfigService service, BervanViewConfig bervanViewConfig) {
         super(new InterviewAppPageLayout(ROUTE_NAME), service, bervanViewConfig, QuestionConfig.class);
         renderCommonComponents();
-    }
-
-    @Override
-    protected Grid<QuestionConfig> getGrid() {
-        Grid<QuestionConfig> grid = new Grid<>(QuestionConfig.class, false);
-        grid.addColumn(new ComponentRenderer<>(questionConfig -> formatTextComponent(questionConfig.getTableFilterableColumnValue())))
-                .setHeader("Name").setKey("name").setResizable(true).setSortable(true);
-        grid.addColumn(new ComponentRenderer<>(questionConfig -> formatTextComponent(percentStr(questionConfig.getDifficulty1Percent()))))
-                .setHeader("Level 1 %").setKey("difficulty1Percent").setResizable(true);
-        grid.addColumn(new ComponentRenderer<>(questionConfig -> formatTextComponent(percentStr(questionConfig.getDifficulty2Percent()))))
-                .setHeader("Level 2 %").setKey("difficulty2Percent").setResizable(true);
-        grid.addColumn(new ComponentRenderer<>(questionConfig -> formatTextComponent(percentStr(questionConfig.getDifficulty3Percent()))))
-                .setHeader("Level 3 %").setKey("difficulty3Percent").setResizable(true);
-        grid.addColumn(new ComponentRenderer<>(questionConfig -> formatTextComponent(percentStr(questionConfig.getDifficulty4Percent()))))
-                .setHeader("Level 4 %").setKey("difficulty4Percent").setResizable(true);
-        grid.addColumn(new ComponentRenderer<>(questionConfig -> formatTextComponent(percentStr(questionConfig.getDifficulty5Percent()))))
-                .setHeader("Level 5 %").setKey("difficulty5Percent").setResizable(true);
-        grid.addColumn(new ComponentRenderer<>(questionConfig -> formatTextComponent(
-                        questionConfig.getCodingTasksAmount() != null ? String.valueOf(questionConfig.getCodingTasksAmount()) : "0")))
-                .setHeader("Coding Tasks").setKey("codingTasksAmount").setResizable(true);
-        grid.getElement().getStyle().set("--lumo-size-m", 100 + "px");
-
-        return grid;
-    }
-
-    private String percentStr(Integer value) {
-        return value != null ? value + "%" : "0%";
-    }
-
-    @Override
-    protected void buildOnColumnClickDialogContent(Dialog dialog, VerticalLayout dialogLayout, HorizontalLayout headerLayout, String clickedField, QuestionConfig item) {
-        TextFieldBase field;
-
-        switch (clickedField) {
-            case "name" -> {
-                field = new TextField("Name");
-                field.setWidth("100%");
-                field.setValue(item.getTableFilterableColumnValue());
-            }
-            case "difficulty1Percent" -> {
-                field = createPercentField("Level 1 %");
-                field.setValue(item.getDifficulty1Percent());
-            }
-            case "difficulty2Percent" -> {
-                field = createPercentField("Level 2 %");
-                field.setValue(item.getDifficulty2Percent());
-            }
-            case "difficulty3Percent" -> {
-                field = createPercentField("Level 3 %");
-                field.setValue(item.getDifficulty3Percent());
-            }
-            case "difficulty4Percent" -> {
-                field = createPercentField("Level 4 %");
-                field.setValue(item.getDifficulty4Percent());
-            }
-            case "difficulty5Percent" -> {
-                field = createPercentField("Level 5 %");
-                field.setValue(item.getDifficulty5Percent());
-            }
-            case "codingTasksAmount" -> {
-                IntegerField integerField = new IntegerField("Coding Tasks");
-                integerField.setMin(0);
-                integerField.setMax(20);
-                field = integerField;
-                field.setWidth("100%");
-                field.setValue(item.getCodingTasksAmount());
-            }
-            default -> throw new RuntimeException("Invalid column!");
-        }
-
-        Button saveButton = new Button("Save");
-        saveButton.addClassName("option-button");
-
-        saveButton.addClickListener(e -> {
-
-            switch (clickedField) {
-                case "name" -> item.setName((String) field.getValue());
-                case "difficulty1Percent" -> item.setDifficulty1Percent((Integer) field.getValue());
-                case "difficulty2Percent" -> item.setDifficulty2Percent((Integer) field.getValue());
-                case "difficulty3Percent" -> item.setDifficulty3Percent((Integer) field.getValue());
-                case "difficulty4Percent" -> item.setDifficulty4Percent((Integer) field.getValue());
-                case "difficulty5Percent" -> item.setDifficulty5Percent((Integer) field.getValue());
-                case "codingTasksAmount" -> item.setCodingTasksAmount((Integer) field.getValue());
-            }
-
-            grid.getDataProvider().refreshItem(item);
-            service.save(item);
-            dialog.close();
-        });
-
-
-        dialogLayout.add(headerLayout, field, saveButton);
-        dialog.add(dialogLayout);
-        dialog.open();
     }
 
     @Override
@@ -165,10 +69,15 @@ public abstract class AbstractQuestionConfigView extends AbstractBervanTableView
             newQuestionConfig.setDifficulty4Percent(p4);
             newQuestionConfig.setDifficulty5Percent(p5);
             newQuestionConfig.setCodingTasksAmount(codingTasksAmount.getValue() != null ? codingTasksAmount.getValue() : 0);
-            data.add(newQuestionConfig);
-            grid.setItems(data);
-            service.save(data.stream().toList());
-            dialog.close();
+            try {
+                service.save(newQuestionConfig);
+                data.add(newQuestionConfig);
+                grid.setItems(data);
+                dialog.close();
+            } catch (Exception e2) {
+                log.error("Could not save configuration!", e2);
+                showErrorNotification("Could not save configuration!");
+            }
         });
         dialogLayout.add(headerLayout, nameField, difficulty1Percent, difficulty2Percent, difficulty3Percent, difficulty4Percent, difficulty5Percent, codingTasksAmount, sumInfo, saveButton);
     }
